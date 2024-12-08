8b646c6ae5fcfbca14e2a456fee1a8bce8b7212c
blueniarb <blueniarb@gmail.com>
2024-12-08 20:43:56 +0800

    Simplify + Handle Error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Paragraph {
+    name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Article {
+    article: String,
+    author: String,
+    paragraph: Vec<Paragraph>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let json = r#"
+    {
+        "article": "How to work with JSON in Rust",
+        "author": "Nicolas",
+        "paragraph": [
+            {
+                "name": "Starting sentence"
+            },
+            {
+                "name": "Body of the paragraph"
+            },
+            {
+                "name": "End of the paragraph"
+            }
+        ]
+    }"#;
+
+    let parsed: Result<Article, serde_json::Error> = serde_json::from_str(json);
+
+    match parsed {
+        Ok(article) => println!(
+            "\n\nThe name of the first article is : {}",
+            article.paragraph[0].name
+        ),
+        Err(e) => println!("Error: {}", e),
+    }
 }
